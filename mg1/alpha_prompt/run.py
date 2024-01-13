ab79b52655a486faee93e6766f5fcc0a828b7880
ilhamv <variansyah_ilham@gmail.com>
2024-01-13 09:46:33 -0800

    add 361-group problem

--- mg1/alpha_prompt/run.py
+++ mg1/alpha_prompt/run.py
@@ -2,19 +2,19 @@ import openmc
 import numpy as np
 import h5py
 
-#===============================================================================
+# ===============================================================================
 # Data
-#===============================================================================
+# ===============================================================================
 
 G = 2
 J = 2
 N = G + J
 
-v1 = 10.
-v2 = 5.
+v1 = 10.0
+v2 = 5.0
 
-SigmaS11 = 1/2
-SigmaS12 = 1/2
+SigmaS11 = 1 / 2
+SigmaS12 = 1 / 2
 SigmaS21 = 0
 SigmaS22 = 1
 
@@ -24,210 +24,209 @@ SigmaF2 = 1
 SigmaT1 = 2
 SigmaT2 = 3
 
-beta1 = 1/4
-beta2 = 1/8
-beta  = beta1 + beta2
+beta1 = 1 / 4
+beta2 = 1 / 8
+beta = beta1 + beta2
 
 chiP1 = 1.0
 chiP2 = 0.0
 
-chiD11 = 3/4
-chiD12 = 1/4
+chiD11 = 3 / 4
+chiD12 = 1 / 4
 
-chiD21 = 1/2
-chiD22 = 1/2
+chiD21 = 1 / 2
+chiD22 = 1 / 2
 
 lam1 = 0.5
 lam2 = 0.07
 
 
-#===============================================================================
+# ===============================================================================
 # Analytical
-#===============================================================================
+# ===============================================================================
 
-nu_list = np.linspace(1,15,500)
+nu_list = np.linspace(1, 15, 500)
 alpha0 = []
 rat12 = []
 
 for nu in nu_list:
-    M = np.zeros([N,N])
+    M = np.zeros([N, N])
 
-    M[0,0] = v1*(SigmaS11 + chiP1*(1-beta)*nu*SigmaF1 - SigmaT1)
-    M[0,1] = v1*(SigmaS21 + chiP1*(1-beta)*nu*SigmaF2)
-    M[1,0] = v2*(SigmaS12 + chiP2*(1-beta)*nu*SigmaF1)
-    M[1,1] = v2*(SigmaS22 + chiP2*(1-beta)*nu*SigmaF2 - SigmaT2)
+    M[0, 0] = v1 * (SigmaS11 + chiP1 * (1 - beta) * nu * SigmaF1 - SigmaT1)
+    M[0, 1] = v1 * (SigmaS21 + chiP1 * (1 - beta) * nu * SigmaF2)
+    M[1, 0] = v2 * (SigmaS12 + chiP2 * (1 - beta) * nu * SigmaF1)
+    M[1, 1] = v2 * (SigmaS22 + chiP2 * (1 - beta) * nu * SigmaF2 - SigmaT2)
 
-    M[0,G+0] = v1*chiD11*lam1
-    M[0,G+1] = v1*chiD21*lam2
-    M[1,G+0] = v2*chiD12*lam1
-    M[1,G+1] = v2*chiD22*lam2
+    M[0, G + 0] = v1 * chiD11 * lam1
+    M[0, G + 1] = v1 * chiD21 * lam2
+    M[1, G + 0] = v2 * chiD12 * lam1
+    M[1, G + 1] = v2 * chiD22 * lam2
 
-    M[G+0,0] = beta1*nu*SigmaF1
-    M[G+0,1] = beta1*nu*SigmaF2
-    M[G+1,0] = beta2*nu*SigmaF1
-    M[G+1,1] = beta2*nu*SigmaF2
+    M[G + 0, 0] = beta1 * nu * SigmaF1
+    M[G + 0, 1] = beta1 * nu * SigmaF2
+    M[G + 1, 0] = beta2 * nu * SigmaF1
+    M[G + 1, 1] = beta2 * nu * SigmaF2
 
-    M[G+0,G+0] = -lam1
-    M[G+1,G+1] = -lam2
+    M[G + 0, G + 0] = -lam1
+    M[G + 1, G + 1] = -lam2
 
-    M = M[:2,:2]
+    M = M[:2, :2]
 
     alpha, phi = np.linalg.eig(M)
     idx = alpha.argsort()[::-1]
     alpha = alpha[idx]
-    phi = phi[:,idx]
+    phi = phi[:, idx]
 
     alpha0.append(alpha[0])
-    phi   = phi[:,0]
-    phi21 = phi[1]/phi[0]
+    phi = phi[:, 0]
+    phi21 = phi[1] / phi[0]
     rat12.append(phi21)
 
 
-#===============================================================================
+# ===============================================================================
 # Monte Carlo
-#===============================================================================
+# ===============================================================================
 
-nu_list_mc = np.linspace(1,15,10)
-nu_list_mc = np.append(nu_list_mc,24/5)
+nu_list_mc = np.linspace(1, 15, 10)
+nu_list_mc = np.append(nu_list_mc, 24 / 5)
 alpha_mc = []
 rat12_mc = []
 
 for nu in nu_list_mc:
-    #===========================================================================
+    # ===========================================================================
     # Set Library
-    #===========================================================================
+    # ===========================================================================
 
-    groups = openmc.mgxs.EnergyGroups([0.0, 1E-5, 2E7])
+    groups = openmc.mgxs.EnergyGroups([0.0, 1e-5, 2e7])
 
-    uo2_xsdata = openmc.XSdata('uo2', groups, num_delayed_groups=J)
+    uo2_xsdata = openmc.XSdata("uo2", groups, num_delayed_groups=J)
     uo2_xsdata.order = 0
 
-    uo2_xsdata.set_inverse_velocity([1/v1, 1/v2], temperature=294.)
+    uo2_xsdata.set_inverse_velocity([1 / v1, 1 / v2], temperature=294.0)
 
-    uo2_xsdata.set_total([SigmaT1, SigmaT2], temperature=294.)
-    uo2_xsdata.set_absorption([SigmaT1-1, SigmaT2-1], temperature=294.)
+    uo2_xsdata.set_total([SigmaT1, SigmaT2], temperature=294.0)
+    uo2_xsdata.set_absorption([SigmaT1 - 1, SigmaT2 - 1], temperature=294.0)
     scatter_matrix = [[[SigmaS11], [SigmaS12]], [[SigmaS21], [SigmaS22]]]
-    uo2_xsdata.set_scatter_matrix(scatter_matrix, temperature=294.)
-    uo2_xsdata.set_decay_rate([lam1, lam2], temperature=294.)
+    uo2_xsdata.set_scatter_matrix(scatter_matrix, temperature=294.0)
+    uo2_xsdata.set_decay_rate([lam1, lam2], temperature=294.0)
 
-    nu_p  = (1.0 - beta)*nu
-    nu_d1 = beta1*nu
-    nu_d2 = beta2*nu
-    uo2_xsdata.set_prompt_nu_fission([nu_p*SigmaF1, nu_p*SigmaF2], temperature=294.)
-    uo2_xsdata.set_delayed_nu_fission([[nu_d1*SigmaF1, nu_d1*SigmaF2],
-                                       [nu_d2*SigmaF1, nu_d2*SigmaF2]], temperature=294.)
-    uo2_xsdata.set_chi_prompt([chiP1, chiP2], temperature=294.)
-    uo2_xsdata.set_chi_delayed([[chiD11, chiD12], [chiD21, chiD22]], temperature=294.)
-    mg_cross_sections_file = openmc.MGXSLibrary(groups,2)
+    nu_p = (1.0 - beta) * nu
+    nu_d1 = beta1 * nu
+    nu_d2 = beta2 * nu
+    uo2_xsdata.set_prompt_nu_fission(
+        [nu_p * SigmaF1, nu_p * SigmaF2], temperature=294.0
+    )
+    uo2_xsdata.set_delayed_nu_fission(
+        [[nu_d1 * SigmaF1, nu_d1 * SigmaF2], [nu_d2 * SigmaF1, nu_d2 * SigmaF2]],
+        temperature=294.0,
+    )
+    uo2_xsdata.set_chi_prompt([chiP1, chiP2], temperature=294.0)
+    uo2_xsdata.set_chi_delayed([[chiD11, chiD12], [chiD21, chiD22]], temperature=294.0)
+    mg_cross_sections_file = openmc.MGXSLibrary(groups, 2)
     mg_cross_sections_file.add_xsdata(uo2_xsdata)
-    mg_cross_sections_file.export_to_hdf5('mgxs.h5')
+    mg_cross_sections_file.export_to_hdf5("mgxs.h5")
 
-
-    #===========================================================================
+    # ===========================================================================
     # Simulation Input File Parameters
-    #===========================================================================
+    # ===========================================================================
 
     # OpenMC simulation parameters
-    batches   = 50
-    inactive  = 10
-    particles = int(1E6)
+    batches = 50
+    inactive = 10
+    particles = int(1e6)
 
-
-    #===========================================================================
+    # ===========================================================================
     # Exporting to OpenMC materials.xml file
-    #===========================================================================
+    # ===========================================================================
 
     materials = {}
-    materials['uo2'] = openmc.Material(name='uo2')
-    materials['uo2'].set_density('macro', 1.)
-    materials['uo2'].add_macroscopic('uo2')
+    materials["uo2"] = openmc.Material(name="uo2")
+    materials["uo2"].set_density("macro", 1.0)
+    materials["uo2"].add_macroscopic("uo2")
     materials_file = openmc.Materials(materials.values())
-    materials_file.cross_sections = 'mgxs.h5'
+    materials_file.cross_sections = "mgxs.h5"
     materials_file.export_to_xml()
 
-
-    #===========================================================================
+    # ===========================================================================
     # Exporting to OpenMC geometry.xml file
-    #===========================================================================
+    # ===========================================================================
 
     # Instantiate ZCylinder surfaces
-    surf_Z1 = openmc.ZPlane(surface_id=1, z0=0.0, boundary_type='reflective')
-    surf_Z2 = openmc.ZPlane(surface_id=2, z0=5.0, boundary_type='reflective')
+    surf_Z1 = openmc.ZPlane(surface_id=1, z0=0.0, boundary_type="reflective")
+    surf_Z2 = openmc.ZPlane(surface_id=2, z0=5.0, boundary_type="reflective")
 
     # Instantiate Cells
-    cell_F = openmc.Cell(cell_id=1, name='F')
+    cell_F = openmc.Cell(cell_id=1, name="F")
 
     # Use surface half-spaces to define regions
     cell_F.region = +surf_Z1 & -surf_Z2
 
     # Register Materials with Cells
-    cell_F.fill = materials['uo2']
+    cell_F.fill = materials["uo2"]
 
     # Instantiate Universes
-    root = openmc.Universe(universe_id=0, name='root universe', cells=[cell_F])
+    root = openmc.Universe(universe_id=0, name="root universe", cells=[cell_F])
 
     # Instantiate a Geometry, register the root Universe, and export to XML
     geometry = openmc.Geometry(root)
     geometry.export_to_xml()
 
-
-    #===========================================================================
+    # ===========================================================================
     # Exporting to OpenMC settings.xml file
-    #===========================================================================
+    # ===========================================================================
 
     # Instantiate a Settings object, set all runtime parameters, and export to XML
     settings_file = openmc.Settings()
     settings_file.batches = batches
     settings_file.inactive = inactive
     settings_file.particles = particles
-    settings_file.output = {'tallies': False}
+    settings_file.output = {"tallies": False}
     settings_file.alpha_mode = True
     settings_file.prompt_only = True
-    settings_file.energy_mode = 'multi-group'
+    settings_file.energy_mode = "multi-group"
 
     # Create an initial uniform spatial source distribution over fissionable zones
-    bounds = [0., 0., 0., 100., 100., 5.]
+    bounds = [0.0, 0.0, 0.0, 100.0, 100.0, 5.0]
     uniform_dist = openmc.stats.Box(bounds[:3], bounds[3:], only_fissionable=True)
     settings_file.source = openmc.source.Source(space=uniform_dist)
 
     settings_file.export_to_xml()
 
-
-    #===========================================================================
+    # ===========================================================================
     # Exporting to OpenMC tallies.xml file
-    #===========================================================================
+    # ===========================================================================
 
-    energy_filter = openmc.EnergyFilter([0.0, 1E-5, 2E7],filter_id=1)
+    energy_filter = openmc.EnergyFilter([0.0, 1e-5, 2e7], filter_id=1)
 
     # Instantiate the first Tally
-    first_tally = openmc.Tally(tally_id=1, name='first tally')
-    first_tally.scores = ['flux']
+    first_tally = openmc.Tally(tally_id=1, name="first tally")
+    first_tally.scores = ["flux"]
     first_tally.filters = [energy_filter]
 
     # Instantiate a Tallies collection and export to XML
     tallies_file = openmc.Tallies([first_tally])
     tallies_file.export_to_xml()
 
-    openmc.run(mpi_args=['srun','-n','8'])
-    #openmc.run(threads=1)
+    openmc.run(mpi_args=["srun", "-n", "8"])
+    # openmc.run(threads=1)
 
-    with openmc.StatePoint('statepoint.50.h5') as osp:
-        alpha, err  = osp.alpha_eff
-        basic_tally = osp.get_tally(name='first tally')
+    with openmc.StatePoint("statepoint.50.h5") as osp:
+        alpha, err = osp.alpha_eff
+        basic_tally = osp.get_tally(name="first tally")
         flux = basic_tally.get_values(scores=["flux"]).reshape(G)
-        rat = flux[0]/flux[1]
+        rat = flux[0] / flux[1]
         alpha_mc.append(alpha)
         rat12_mc.append(rat)
 
 
-#===============================================================================
+# ===============================================================================
 # Plot
-#===============================================================================
+# ===============================================================================
 
-with h5py.File('result.h5', 'w') as hdf:
-    hdf.create_dataset('nu_list', data=nu_list)
-    hdf.create_dataset('nu_list_mc', data=nu_list_mc)
-    hdf.create_dataset('alpha0', data=alpha0)
-    hdf.create_dataset('rat12', data=rat12)
-    hdf.create_dataset('alpha_mc', data=alpha_mc)
-    hdf.create_dataset('rat12_mc', data=rat12_mc)
+with h5py.File("result.h5", "w") as hdf:
+    hdf.create_dataset("nu_list", data=nu_list)
+    hdf.create_dataset("nu_list_mc", data=nu_list_mc)
+    hdf.create_dataset("alpha0", data=alpha0)
+    hdf.create_dataset("rat12", data=rat12)
+    hdf.create_dataset("alpha_mc", data=alpha_mc)
+    hdf.create_dataset("rat12_mc", data=rat12_mc)
---
+++ shem_361/alpha/run.py
@@ -0,0 +1,241 @@
+import openmc
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+# ===============================================================================
+# Data
+# ===============================================================================
+
+# Load material data
+with np.load("../SHEM-361.npz") as data:
+    SigmaT = data["SigmaT"]
+    SigmaC = data["SigmaC"]
+    SigmaS = data["SigmaS"]
+    nuSigmaF_p = data["nuSigmaF_p"]
+    SigmaF = data["SigmaF"]
+    nu_p = data["nu_p"]
+    nu_d = data["nu_d"]
+    chi_p = data["chi_p"]
+    chi_d = data["chi_d"]
+    G = data["G"]
+    J = data["J"]
+    E = data["E"]
+    v = data["v"]
+    lamd = data["lamd"]
+G = G.item()
+J = J.item()
+SigmaA = SigmaT - np.sum(SigmaS, axis=0)
+chi_p = np.sum(chi_p / np.sum(chi_p), axis=1)
+nuSigmaF_p = np.outer(chi_p, SigmaF * nu_p)
+
+# ===============================================================================
+# Analytical
+# ===============================================================================
+
+N = 100
+leak_list = np.linspace(0, 0.005, N)
+results_alpha = np.zeros(N)
+results_phi = np.zeros([N, G])
+results_tr = np.zeros(N)
+results_k = np.zeros(N)
+
+for n in range(N):
+    leak = leak_list[n]
+
+    # The matrix
+    A = np.zeros([G + J, G + J])
+
+    # Top-left [GxG]: phi --> phi
+    A[:G, :G] = SigmaS + nuSigmaF_p - np.diag(SigmaT) - np.diag(np.ones(G) * leak)
+
+    # Top-right [GxJ]: C --> phi
+    A[:G, G:] = np.multiply(chi_d, lamd)
+
+    # Bottom-left [JxG]: phi --> C
+    A[G:, :G] = np.multiply(nu_d, SigmaF)
+
+    # bottom-right [JxJ]: C --> C
+    A[G:, G:] = -np.diag(lamd)
+
+    # Multiply with neutron speed
+    M = np.copy(A)
+    M[:G, :] = np.dot(np.diag(v), A[:G, :])
+
+    alpha, phi = np.linalg.eig(M)
+    idx = alpha.argsort()[::-1]
+    alpha = alpha[idx][0].real
+    phi = phi[:, idx]
+    phi = phi[:, 0][:G].real
+    phi /= np.sum(phi)
+
+    tr = np.dot(phi, 1.0 / v) / np.dot(phi, SigmaA + leak)
+    k = np.dot(phi, nu_p * SigmaF + np.sum(np.multiply(nu_d, SigmaF), axis=0)) / np.dot(
+        phi, SigmaA + leak
+    )
+
+    results_alpha[n] = alpha
+    results_phi[n, :] = phi
+    results_tr[n] = tr
+    results_k[n] = k
+
+# ===============================================================================
+# Monte Carlo
+# ===============================================================================
+
+N = 20
+leak_list_mc = np.linspace(0, 0.005, N)
+results_alpha_mc = np.zeros(N)
+results_phi_mc = np.zeros([N, G])
+results_tr_mc = np.zeros(N)
+results_k_mc = np.zeros(N)
+
+SigmaS = SigmaS.transpose()
+SigmaS = SigmaS[:, :, np.newaxis]
+
+for n in range(N):
+    leak = leak_list_mc[n]
+
+    # ===========================================================================
+    # Set Library
+    # ===========================================================================
+
+    groups = openmc.mgxs.EnergyGroups(openmc.mgxs.GROUP_STRUCTURES["SHEM-361"])
+
+    uo2_xsdata = openmc.XSdata("uo2", groups, num_delayed_groups=J)
+    uo2_xsdata.order = 0
+
+    uo2_xsdata.set_inverse_velocity(1 / v, temperature=294.0)
+
+    uo2_xsdata.set_total(SigmaT + leak, temperature=294.0)
+    uo2_xsdata.set_absorption(SigmaA + leak, temperature=294.0)
+
+    uo2_xsdata.set_scatter_matrix(SigmaS, temperature=294.0)
+    uo2_xsdata.set_decay_rate(lamd, temperature=294.0)
+
+    uo2_xsdata.set_prompt_nu_fission(nu_p * SigmaF, temperature=294.0)
+    uo2_xsdata.set_delayed_nu_fission(
+        np.multiply(nu_d, SigmaF),
+        temperature=294.0,
+    )
+    uo2_xsdata.set_chi_prompt(chi_p, temperature=294.0)
+    uo2_xsdata.set_chi_delayed(chi_d.transpose(), temperature=294.0)
+    mg_cross_sections_file = openmc.MGXSLibrary(groups, J)
+    mg_cross_sections_file.add_xsdata(uo2_xsdata)
+    mg_cross_sections_file.export_to_hdf5("mgxs.h5")
+
+    # ===========================================================================
+    # Simulation Input File Parameters
+    # ===========================================================================
+
+    # OpenMC simulation parameters
+    batches = 50
+    inactive = 10
+    particles = int(1e6)
+
+    # ===========================================================================
+    # Exporting to OpenMC materials.xml file
+    # ===========================================================================
+
+    materials = {}
+    materials["uo2"] = openmc.Material(name="uo2")
+    materials["uo2"].set_density("macro", 1.0)
+    materials["uo2"].add_macroscopic("uo2")
+    materials_file = openmc.Materials(materials.values())
+    materials_file.cross_sections = "mgxs.h5"
+    materials_file.export_to_xml()
+
+    # ===========================================================================
+    # Exporting to OpenMC geometry.xml file
+    # ===========================================================================
+
+    # Instantiate ZCylinder surfaces
+    surf_Z1 = openmc.ZPlane(surface_id=1, z0=0.0, boundary_type="reflective")
+    surf_Z2 = openmc.ZPlane(surface_id=2, z0=5.0, boundary_type="reflective")
+
+    # Instantiate Cells
+    cell_F = openmc.Cell(cell_id=1, name="F")
+
+    # Use surface half-spaces to define regions
+    cell_F.region = +surf_Z1 & -surf_Z2
+
+    # Register Materials with Cells
+    cell_F.fill = materials["uo2"]
+
+    # Instantiate Universes
+    root = openmc.Universe(universe_id=0, name="root universe", cells=[cell_F])
+
+    # Instantiate a Geometry, register the root Universe, and export to XML
+    geometry = openmc.Geometry(root)
+    geometry.export_to_xml()
+
+    # ===========================================================================
+    # Exporting to OpenMC settings.xml file
+    # ===========================================================================
+
+    # Instantiate a Settings object, set all runtime parameters, and export to XML
+    settings_file = openmc.Settings()
+    settings_file.batches = batches
+    settings_file.inactive = inactive
+    settings_file.particles = particles
+    settings_file.output = {"tallies": False}
+    settings_file.alpha_mode = True
+    settings_file.energy_mode = "multi-group"
+
+    # Create an initial uniform spatial source distribution over fissionable zones
+    bounds = [0.0, 0.0, 0.0, 100.0, 100.0, 5.0]
+    uniform_dist = openmc.stats.Box(bounds[:3], bounds[3:], only_fissionable=True)
+    settings_file.source = openmc.source.Source(space=uniform_dist)
+
+    settings_file.export_to_xml()
+
+    # ===========================================================================
+    # Exporting to OpenMC tallies.xml file
+    # ===========================================================================
+
+    energy_filter = openmc.EnergyFilter(
+        openmc.mgxs.GROUP_STRUCTURES["SHEM-361"], filter_id=1
+    )
+
+    # Instantiate the first Tally
+    first_tally = openmc.Tally(tally_id=1, name="first tally")
+    first_tally.scores = ["flux"]
+    first_tally.filters = [energy_filter]
+
+    # Instantiate a Tallies collection and export to XML
+    tallies_file = openmc.Tallies([first_tally])
+    tallies_file.export_to_xml()
+
+    openmc.run(mpi_args=["srun", "-n", "8"])
+    # openmc.run(threads=1)
+
+    with openmc.StatePoint("statepoint.50.h5") as osp:
+        alpha, err = osp.alpha_eff
+        basic_tally = osp.get_tally(name="first tally")
+        flux = basic_tally.get_values(scores=["flux"]).reshape(G)
+        results_alpha_mc[n] = alpha
+        results_phi_mc[n, :] = flux
+
+    with h5py.File("statepoint.50.h5", "r") as f:
+        tr = f["alpha_mode_tallies/removal_time"][0]
+        k = f["alpha_mode_tallies/k_effective"][0]
+        results_tr_mc[n] = tr
+        results_k_mc[n] = k
+
+
+# ===============================================================================
+# Plot
+# ===============================================================================
+
+with h5py.File("result.h5", "w") as hdf:
+    hdf.create_dataset("leak_list", data=leak_list)
+    hdf.create_dataset("leak_list_mc", data=leak_list_mc)
+    hdf.create_dataset("alpha0", data=results_alpha)
+    hdf.create_dataset("phi", data=results_phi)
+    hdf.create_dataset("tr", data=results_tr)
+    hdf.create_dataset("k", data=results_k)
+    hdf.create_dataset("alpha0_mc", data=results_alpha_mc)
+    hdf.create_dataset("phi_mc", data=results_phi_mc)
+    hdf.create_dataset("tr_mc", data=results_tr_mc)
+    hdf.create_dataset("k_mc", data=results_k_mc)
+    hdf.create_dataset("E", data=E)
